59fd6fa346ef7ead4c66e3b379a1cf9d3a41fe52
Gianmarco G <giangarof98@gmail.com>
2022-02-09 20:04:53 -0500

    added login/register toggle

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,5 @@
 <template>
-   <!-- Header -->
-  <header id="header" class="bg-gray-700">
-    <nav class="container mx-auto flex justify-start items-center py-5 px-4">
-      <!-- App Name -->
-      <a class="text-white font-bold uppercase text-2xl mr-4" href="#">Music</a>
-
-      <div class="flex flex-grow items-center">
-        <!-- Primary Navigation -->
-        <ul class="flex flex-row mt-1">
-          <!-- Navigation Links -->
-          <li>
-            <a class="px-2 text-white" href="#">Login / Register</a>
-          </li>
-          <li>
-            <a class="px-2 text-white" href="#">Manage</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  </header>
+    <AppHeader />
 
   <!-- Introduction -->
   <section class="mb-8 py-20 text-white text-center relative">
@@ -403,12 +384,23 @@
       </div>
     </div>
   </div>
+  
+ <AppAuth />
+
  </template>
+
 
 <script>
 
+import AppHeader from './components/Header.vue'
+import AppAuth from './components/Auth.vue'
+
+export default{
+    name:'App',
+    components: {
+      AppHeader,
+      AppAuth
+    },
+  }
+
 </script>
-
-<style>
-
-</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
 import './assets/main.css'
 import './assets/tailwind.css'
 
-createApp(App).mount('#app')
+let app = createApp(App)
+app.use(store)
+app.use(router)
+app.mount('#app')
+
