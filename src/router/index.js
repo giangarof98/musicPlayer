0a6c41b3ee70f653db703216593d16dccc229e87
Gianmarco G <giangarof98@gmail.com>
2022-02-10 23:55:09 -0500

    router started

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,31 @@
+import { createRouter, createWebHistory } from "vue-router";
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
+import Manage from '@/views/Manage.vue'
+
+
 const routes =[
     {
-
+        name: 'home',
+        path: '/',
+        component: Home
+    },
+    {
+        name:'about',
+        path: '/about',
+        component: About
+    },
+    {
+        name:'manage',
+        path: '/manage',
+        component: Manage
     }
 ]
 
-export default routes
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+    linkExactActiveClass: 'text-yellow-500'
+})
+
+export default router
